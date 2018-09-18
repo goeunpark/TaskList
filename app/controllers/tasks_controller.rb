0d855e12c4b7fa50e4325bdfb80f9f65fa289994
Goeun Park <goeuntothepark@gmail.com>
2018-09-18 15:44:36 -0700

    added 2 tasks to database, prelim css

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.all.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+    end
+
+  end
+
+end
---
+++ db/migrate/20180918183511_add_completion_date.rb
@@ -0,0 +1,7 @@
+class AddCompletionDate < ActiveRecord::Migration[5.2]
+  def change
+
+    add_column(:tasks, :completed_date, :date)
+
+  end
+end
