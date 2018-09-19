4b4912f403687c94e2d2c25caeb1336b0f8e4302
Goeun Park <goeuntothepark@gmail.com>
2018-09-19 15:53:54 -0700

    add new and create methods in TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,17 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.all.find_by(id: task_id)
 
-
     if @task.nil?
       head :not_found
     end
 
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(action: params[:task][:action], completed_date: params[:task][:completed_date])
+  end
 end
