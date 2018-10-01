627f7ce80328640a634f7e23e7e78e3f1605477e
Goeun Park <goeuntothepark@gmail.com>
2018-09-30 18:36:01 -0700

    change create to take in task_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(action: params[:task][:action], completed_date: params[:task][:completed_date])
+    @task = Task.new(task_params)
 
     if @task.save
       redirect_to tasks_path
@@ -33,9 +33,13 @@ class TasksController < ApplicationController
 
   def update
     task = Task.find(params[:id])
-    task.update(task_params)
+    result = task.update(task_params)
 
-    redirect_to task_path(task.id)
+    if result
+      redirect_to task_path(task.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
